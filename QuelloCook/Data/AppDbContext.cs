9d9c6585131bfb66165ba41067158103ef896f4e
itsquelc <raquelnunescirino@gmail.com>
2024-08-14 14:39:12 -0300

    continuacao e criancao de banco

---
+++ QuelloCook/Data/AppDbContext.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using QuelloCook.Models;
+
+namespace QuelloCook.Data;
+
+    public class AppDbContext : IdentityDbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext>options) : base(options) 
+        {
+
+        }
+
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Comentario> Comentarios { get; set; }
+        public DbSet<Ingrediente> Ingredientes { get; set; }
+        public DbSet<Receita> Receitas { get; set; }
+        public DbSet<ReceitaIngrediente> ReceitaIngredientes { get; set; }
+        public DbSet<Usuario> Usuarios { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+
+        builder.Entity<ReceitaIngrediente>()
+        .HasKey(ri => new { ri.ReceitaId, ri.IngredientesId });
+    }
+}
